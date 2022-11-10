994f531c17e37f98c57771be35dc9ceab45b237b
EmilyJarecki <jareckiemily@yahoo.com>
2022-11-10 15:30:42 -0600

    timer added

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+const tick = document.querySelector("#timerCount");
+const startTimer = document.querySelector("#startTimer");
+
+function reset() {
+  clearInterval(interval);
+  tick.textContent = 0;
+  startTimer.disabled = false;
+}
+
+function updateTicks(num) {
+  tick.textContent = num;
+}
+
+let count = Number(tick.textContent);
+startTimer.addEventListener("click", function(){
+  interval = setInterval(() => {
+    count += 1;
+    updateTicks(count); 
+  }, 1000);
+  startTimer.disabled = true;
+});
+
+document.querySelector("#reset").addEventListener("click", reset);
+
+
+
